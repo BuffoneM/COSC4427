78933d2bedfd7ebb0f0014d9e2dda14b47a4e9b7
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-05-20 22:26:42 -0400

    +

--- Assignments/Queens.py
+++ Assignments/Queens.py
@@ -18,7 +18,20 @@ class Puzzle:
         self.board = [[0 for i in range(n)] for j in range(n)]
         
     # Method to find a safe place
-    #def findASafePlace(self):
+    def solvePuzzle(self, column):
+        if column >= self.n:
+            return True
+        
+        for i in range(self.n):
+            if self.isSafe(i, column):
+                self.placeQueen(i, column)
+                
+            if self.solvePuzzle(column + 1) == True:
+                return True
+            
+            self.removeQueen(i, column)
+            
+        return False
     
     # Method to check if a position is safe
     def isSafe(self, row, column):
@@ -29,15 +42,25 @@ class Puzzle:
                 return False
             
         startingPos = min(row, column) - 1
-        # Check the diagonals
-        #for i in range(startingPos, )
         
-    
+        # Check the left diagonals (zip function combines row and column together)
+        for i,j in zip(range(row, -1, -1), range(column, -1, -1)):
+            if(self.board[i][j] == 1):
+                return False
+            
+        for i,j in zip(range(row, self.n, 1), range(column, -1, -1)):
+            if(self.board[i][j] == 1):
+                return False
+            
+        return True
+            
     # Method to place a queen on the board
-    #def placeQueen(self):
+    def placeQueen(self, x, y):
+        self.board[x][y] = 1
     
     # Method to remove a queen on the board
-    #def removeQueen(self):
+    def removeQueen(self, x, y):
+        self.board[x][y] = 0
         
     # Method to print the board
     def displayBoard(self):
@@ -46,14 +69,18 @@ class Puzzle:
                 for j in range(self.n):
                     print(self.board[i][j], " ", end="")
                 print()
-        
-
-     
+            
 def main():
     print("-----")
     # Collect the size from the user and create the puzzle board
     puzzle1 = Puzzle(int(input("Enter a board of size N: ")))
     puzzle1.displayBoard()
+    
+    if puzzle1.solvePuzzle(0):
+        print("Solution found!")
+        puzzle1.displayBoard()
+    
+    
     print("-----")
     
 if __name__ == "__main__":
