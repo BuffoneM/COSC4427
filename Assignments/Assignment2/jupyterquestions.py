3caeea56d0bf83317d4c2d3ea67ef0e3c2265c41
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-06-08 17:38:18 -0400

    +

---
+++ Assignments/Assignment2/jupyterquestions.py
@@ -0,0 +1,39 @@
+import random
+import math
+
+num_list = [random.randrange(1,500) for i in range(0, 10)]
+print(num_list)
+
+largestValue = max(num_list)
+print("Largest:", largestValue)
+
+smallestValue = min(num_list)
+print("Smallest:", smallestValue)
+
+temp = max(num_list)
+num_list.remove(max(num_list))
+secLargestValue = max(num_list)
+print("2nd largest:", secLargestValue)
+num_list.append(temp)
+
+temp = min(num_list)
+num_list.remove(min(num_list))
+secSmallestValue = min(num_list)
+print("2nd smallest:", secSmallestValue)
+num_list.append(temp)
+
+print(num_list)
+total = sum(num_list)
+avg = total/len(num_list)
+print("Sum:", total, "\nAverage:", avg)
+
+sum = 0
+for num in num_list:
+    sum += (num - avg) ** 2
+stdDev = math.sqrt(sum / (len(num_list) - 1))
+print("Standard deviation:", stdDev)
+
+num_list.sort()
+print("Sorted list:", num_list)
+median = num_list[int((len(num_list) - 1) / 2)]
+print("Median:", median)
---
+++ Assignments/Assignment2/jupyterquestionsnumpy.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+num_list = np.random.randint(1, 500, 10)
+print(num_list)
+
+largestInteger = np.max(num_list)
+print(largestInteger)
+
+smallestInteger = np.min(num_list)
+print(smallestInteger)
+
+secLargestValue = num_list[(np.argsort(num_list)[-2:])[0]]
+print("2nd largest:", secLargestValue)
+print(num_list)
+
+secSmallestValue = num_list[(np.argsort(num_list)[1])]
+print("2nd smallest:", secSmallestValue)
+
+
+
+#print(num_list)
+#total = sum(num_list)
+#avg = total/len(num_list)
+#print("Sum:", total, "\nAverage:", avg)
+
+#stdDev = 0
+#print("Standard deviation:", stdDev)
+
+#print("Sorted list:", num_list)
+#median = 0
+#print("Median:", median)
+
